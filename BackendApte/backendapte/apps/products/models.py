36e4b08791a0f11a21af803356679ecafc95ac01
unknown <preiraclement79@gmail.com>
2025-09-29 17:36:41 +0100

    Maj

--- BackendApte/backendapte/apps/products/models.py
+++ BackendApte/backendapte/apps/products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     """
-    Catégories de produits (ex: Caméras, Accessoires...).
+    Catégories de produits (ex: Caméras).
     """
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
