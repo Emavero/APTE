25ecb8933f64f8398859d95117ac3722380819bc
Emavero <preiraclement79@gmail.com>
2025-10-28 21:43:12 +0100

    Maj

--- BackendApte/backendapte/manage.py
+++ BackendApte/backendapte/manage.py
@@ -2,13 +2,17 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from typing import TYPE_CHECKING
 
+if TYPE_CHECKING:
+    # Help static analyzers resolve imports without importing Django at runtime.
+    from django.core.management import execute_from_command_line # type: ignore
 
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backendapte.settings.dev')
     try:
-        from django.core.management import execute_from_command_line
+        from django.core.management import execute_from_command_line # type: ignore
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
